ada467ee8762798a9125209c9a481da7d2887c7d
Suryanshu Patnaik <er.suryanshupatnaik.4@gmail.com>
2023-01-21 12:22:12 +0530

    main

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const accordion = document.querySelectorAll(".accordion");
+
+accordion.forEach((element) => {
+	element.addEventListener("click", () => {
+		element.classList.toggle("active");
+		let panel = element.nextElementSibling;
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	});
+});
